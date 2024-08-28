c0e883a111b04389ca6b570790314919c964b449
XpFinder <xpbinning@gmail.com>
2024-08-28 20:02:02 +0530

    Added a new random response generator
    Created a new Function expression getComputerChoice() that
    generates random responses with the help of math.random()
    method.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+
+let getComputerChoice = () => {
+    let randNum = Math.floor(Math.random() * 100)+1;
+
+    if(randNum <= 33){
+        return "rock";
+    }
+    else if(randNum > 33 && randNum <= 66){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
